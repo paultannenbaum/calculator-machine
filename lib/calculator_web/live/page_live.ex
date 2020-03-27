fdcbff0cac14403b6146fa60516349c1d9b577e6
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-03-27 12:05:20 -0700

    Add liveview event listeners

--- lib/calculator_web/live/page_live.ex
+++ lib/calculator_web/live/page_live.ex
@@ -2,35 +2,22 @@ defmodule CalculatorWeb.PageLive do
   use CalculatorWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, readout: 0, machine_state: 'idle')}
   end
 
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("number_click", %{"number" => number}, socket) do
+    {:noreply, assign(socket, readout: number)}
   end
 
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
+  def handle_event("decimal_click", socket) do
+    {:noreply, socket}
   end
 
-  defp search(query) do
-    if not CalculatorWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
+  def handle_event("cancel_click", socket) do
+    {:noreply, socket}
+  end
 
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  def handle_event("operator_click", %{"operator" => operator}, socket) do
+    {:noreply, socket}
   end
 end
